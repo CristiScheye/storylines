8d4cec8813bca692f40a236ad7d15bdb841aa28c
Cristi Scheye <cristi.scheye@gmail.com>
2014-01-26 17:45:47 -0800

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+
+
+gem 'rails', '4.0.2'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'haml-rails'
+gem 'bootstrap-sass'
+gem 'pg'
+
+group :development do
+  gem 'thin'
+  gem 'pry'
+  gem 'therubyracer', platforms: :ruby
+end
+
+
+group :production do
+  gem 'rails_12factor'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Storyline::Application.config.secret_key_base = '0d3385f737d771d54d1278ffb64a414e18b9e45515d7ab7c8d5e1b196ba36afccc26a44b0a9b27b0fe5228d57ece3fd94034785b92e7b77f5a07239f9c5e529d'

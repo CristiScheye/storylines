154043b31cfcf1df297ba45fd9b419b13c210723
Cristi Scheye <cristi.scheye@gmail.com>
2014-02-08 16:09:00 -0800

    Adding nested create entry resource to story, creating form for entry object

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,13 @@
 class EntriesController < ApplicationController
+  def create
+    @story = Story.find(params[:story_id])
+    @entry = @story.entries.build(params.require(:entry).permit(:user_id, :body))
+
+    if @entry.save
+      flash[:success] = 'Your entry was saved'
+      redirect_to Story.rand_unfinished
+    else
+      render 'stories/show', error: 'Your entry was not saved'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Storyline::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
   get '/about', controller: 'application', action: 'about'
 
-  resources :stories, only: [:new, :create, :show, :index]
+  resources :stories, only: [:new, :create, :show, :index] do
+    resources :entries, only: [:create]
+  end
 
 end

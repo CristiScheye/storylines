1646eaf80ef05ec69433a331db9491c89d24bb2b
Cristi Scheye <cristi.scheye@gmail.com>
2014-02-14 14:06:43 -0800

    Removing superfluous code

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,17 +17,6 @@ Storyline::Application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
-  config.action_mailer.smtp_settings = {
-     :address              => "smtp.gmail.com",
-     :port                 => 587,
-     :domain               => 'domain.com',
-     :user_name            => '.com',
-     :password             => 'password',
-     :authentication       => :plain,
-     :enable_starttls_auto => true  }
-
-     
-
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {

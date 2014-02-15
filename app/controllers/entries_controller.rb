af47efb605c37b67c4bb699bdf64325dcff46217
Cristi Scheye <cristi.scheye@gmail.com>
2014-02-14 20:36:09 -0800

    Minor clean-up

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,14 +4,13 @@ class EntriesController < ApplicationController
   def create
 
     @story = Story.find(params[:story_id])
-    @entry = @story.entries.build(params.require(:entry).permit(:body))
-    @entry.user_id = current_user.id
+    @entry = @story.entries.build(params.require(:entry).permit(:body, :user_id))
 
     if @entry.save
-      flash[:success] = 'Your entry was saved'
+      flash[:success] = 'Your entry was saved. Write another!'
       redirect_to Story.rand_unfinished
     else
-      render 'stories/show', error: 'Your entry was not saved'
+      render 'stories/show', error: 'Hmmm... Something went wrong. Your entry was not saved.'
     end
   end
 end

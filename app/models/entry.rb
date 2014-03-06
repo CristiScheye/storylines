ae8bb1d653973681356674452f4b9a7f361aec80
Cristi Scheye <cristi.scheye@gmail.com>
2014-03-06 14:05:06 -0800

    changing max from timestamp (not pg compatible?) to id

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -4,6 +4,6 @@ class Entry < ActiveRecord::Base
   validates :body, length: {minimum: 3, maximum: 150}
 
   def self.previous_by_users_other_than(user)
-    where(id: Entry.select("max(entries.created_at)").group(:story_id)).where.not(user: user)
+    where(id: Entry.select("max(entries.id)").group(:story_id)).where.not(user: user)
   end
 end

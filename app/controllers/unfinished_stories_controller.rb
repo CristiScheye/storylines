a27f26f226f09956927c62d81d507da6481bab33
Cristi Scheye <cristi.scheye@gmail.com>
2014-03-19 17:06:58 -0700

    switching the order of unstarted/unfinished story retrieval

--- app/controllers/unfinished_stories_controller.rb
+++ app/controllers/unfinished_stories_controller.rb
@@ -1,8 +1,9 @@
 class UnfinishedStoriesController < ApplicationController
   before_action :authenticate_user!, only: [:show]
   def show
-    random_story = Story.for_prev_entries_not_by(current_user).unfinished.sample 
-    random_story ||= Story.for_users_other_than(current_user).unstarted.sample
+    random_story = Story.for_users_other_than(current_user).unstarted.sample
+      || Story.for_prev_entries_not_by(current_user).unfinished.sample 
+
 
     if random_story.nil?
       flash[:notice] = "Sorry, no unfinished stories to add to right now. Would you like to start a new one?"
